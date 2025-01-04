e26d4592daa46e33d5f7121e45fd83724a3f4a5f
CHIGODI <chiegody254@gmail.com>
2025-01-04 13:07:23 +0300

    Styled skills on both large and small screens

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,34 +1,36 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHtml5, faPython, faJs, faCss3 } from '@fortawesome/free-brands-svg-icons'
+import { faHtml5, faPython, faJs, faReact } from '@fortawesome/free-brands-svg-icons'
 import { faCode } from '@fortawesome/free-solid-svg-icons'
 
 const Skills = () => {
     return (
         <div id="projects">
-            <h2 class="sub-title">Skills</h2>
-            <p id="sub-title-desc">My Technical Skills, Languages and Tools</p>
+            <div className="skill-headings">
+                <h2 class="sub-title">Skills</h2>
+                <p id="sub-title-desc">My Technical Skills, Languages and Tools</p>
+            </div>
             <div class="container">
                 <div className="languages">
                     <div className="ln-title">
-                        <h3 className="text-start">Languages</h3>
-                        <p className="text-start">Tech stack that I utilise</p>
-                    </div>
-                    <div id="ln-tiles">
-                        <FontAwesomeIcon icon={faHtml5} className="ln-icons" />
-                        <h4>HTML</h4>
-                    </div>
-                    <div id="ln-tiles">
-                        <FontAwesomeIcon icon={faCss3} className="ln-icons" />
-                        <h4>CSS</h4>
+                        <h3>Languages & Frameworks</h3>
+                        <p>Tech stack that I utilise</p>
                     </div>
                     <div id="ln-tiles">
                         <FontAwesomeIcon icon={faJs} className="ln-icons" />
-                        <h4>JavaScript</h4>
+                        <h4>Next.js</h4>
+                    </div>
+                    <div id="ln-tiles">
+                        <FontAwesomeIcon icon={faReact} className="ln-icons" />
+                        <h4>React</h4>
+                    </div>
+                    <div id="ln-tiles">
+                        <FontAwesomeIcon icon={faJs} className="ln-icons" />
+                        <h4>Django</h4>
                     </div>
                     <div id="ln-tiles">
                         <FontAwesomeIcon icon={faPython} className="ln-icons" />
-                        <h4>Python</h4>
+                        <h4>Flask</h4>
                     </div>
                     <div id="ln-tiles">
                          <FontAwesomeIcon icon={faCode}  className="ln-icons" />
@@ -37,23 +39,23 @@ const Skills = () => {
                 </div>
                 <div className="tools">
                     <div className="ln-title">
-                        <h3 className="text-end" style={{ color: "#31304D" }}>Tools</h3>
-                        <p className="text-end" style={{ color: "#555555" }}>Tools that I know and use daily</p>
+                        <h3>Development & collaboration Tools</h3>
+                        <p>Tools that I know and use daily</p>
                     </div>
                     <div id="ln-tiles">
-                        <h4>VS Code</h4>
+                        <h4>Trello</h4>
+                    </div>
+                    <div id="ln-tiles">
+                        <h4>Discord</h4>
+                    </div>
+                    <div id="ln-tiles">
+                        <h4>Postman</h4>
                     </div>
                     <div id="ln-tiles">
                         <h4>Docker</h4>
                     </div>
                     <div id="ln-tiles">
-                        <h4>Linux</h4>
-                    </div>
-                    <div id="ln-tiles">
-                        <h4>Emacs</h4>
-                    </div>
-                    <div id="ln-tiles">
-                        <h4>Github</h4>
+                        <h4>Git & Github</h4>
                     </div>
                 </div>
 
